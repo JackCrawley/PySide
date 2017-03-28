c62724003e6e247a38c4925aa78da1f96b254698
Jack Crawley <signups@jackcrawley.com>
2017-03-28 19:09:49 +0100

    Fix for QtCore.MSG type - added missing fields hParam and message.
    Applied patch with name:
    MSG-wparam-and-message-types-Fix-PYSIDE84

--- PySide/pysidewtypes.h
+++ PySide/pysidewtypes.h
@@ -2,10 +2,17 @@
 #define __PYSIDEWTYPES__
 
 typedef struct HWND__ *HWND;
-typedef unsigned UINT;
+typedef unsigned int UINT;
 typedef long LONG;
 typedef unsigned long DWORD;
-typedef UINT WPARAM;
+
+#if defined(_WIN64)
+typedef unsigned __int64 UINT_PTR;
+#else
+typedef unsigned int UINT_PTR;
+#endif
+
+typedef UINT_PTR WPARAM;
 typedef LONG LPARAM;
 
 struct POINT
